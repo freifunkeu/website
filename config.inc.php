d6503c69568874b54115ce2d17aac7a6c41fdaee
ArwedSchmidt <ArwedSchmidt@users.noreply.github.com>
2017-06-22 23:13:16 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,4 @@
 <?php
 $site_tld="ffeu";
-$firmware_prefix = 'gluon-'.$site_tld.'-stable-2015.1.2-0';
+$firmware_prefix = 'gluon-'.$site_tld.'-fastd-2016.2.4-0.9.2+exp20170320';
 $show_community_selector=false;
-
-
