72bab637a88a3caf037f200b73f488bab16b2b84
ArwedSchmidt <ArwedSchmidt@users.noreply.github.com>
2017-10-02 22:19:41 +0200

    [FIX] Update image download configuration
    [FIX] Image location has changed slightly

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php
 $site_tld="ffeu";
 $firmware_prefix = 'gluon-'.$site_tld.'-fastd-2016.2.4-0.9.2+exp20170320';
-$images_baseurl = "https://images.ffeu.de";
+$images_baseurl = "https://images.ffeu.de/euskirchen";
 $show_community_selector=false;
