4001e89e2a41094f225eadab381bc3b2a12a7b98
ArwedSchmidt <ArwedSchmidt@users.noreply.github.com>
2017-10-02 22:23:05 +0200

    [FIX] Updated nasty hardcoded file prefix..

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php
 $site_tld="ffeu";
-$firmware_prefix = 'gluon-'.$site_tld.'-fastd-2016.2.4-0.9.2+exp20170320';
+$firmware_prefix = 'gluon-'.$site_tld.'-fastd-2016.2.7-0.9.6+exp20170825';
 $images_baseurl = "https://images.ffeu.de/euskirchen";
 $show_community_selector=false;
