b2166bcea7465f30ddd0751c1a4f43b065f68031
K3V1N <k3v1n-eu@users.noreply.github.com>
2019-10-04 23:58:54 +0200

    Update participate_member.php

--- content/participate_member.php
+++ content/participate_member.php
@@ -1,13 +1,15 @@
 <?php
 require_once "config.inc.php";
 ?>
-
-    <h3>Firmware-Download</h3>
-<a href="https://images.ffkbu.de/euskirchen/stable-v2/" target="_blank">Stable Firmware</a>
-<a href="https://images.ffkbu.de/euskirchen/stable-legacy/" target="_blank">Legacy Firmware</a>
-<a href="https://images.ffkbu.de/euskirchen/beta-v2/" target="_blank">Beta Firmware</a>
-
 <aside class="participate_list-links">
+<h3>Firmware-Download</h3>
+    <ul>
+        <li><a href="https://images.ffkbu.de/euskirchen/stable-v2/" target="_blank">Stable Firmware</a></li>
+        <li><a href="https://images.ffkbu.de/euskirchen/stable-legacy/" target="_blank">Legacy Firmware</a></li>
+        <li><a href="https://images.ffkbu.de/euskirchen/beta-v2/" target="_blank">Beta Firmware</a></li>
+    </ul>
+
+
     <h2>Links zu Freifunk Euskirchen</h2>
     <ul>
         <li><a target="_blank" href="https://kbu.freifunk.net/wiki/index.php?title=Hauptseite">Freifunk KBU Wiki</a> - Unser gesammeltes Wissen</li>
